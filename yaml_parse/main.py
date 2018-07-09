7b2f3e5ed1d5d2c9d23b7cedc43152fab21a63d7
Ben Coleman <benjamin.coleman@me.com>
2018-07-09 16:31:57 +0100

    food menuw

--- yaml_parse/main.py
+++ yaml_parse/main.py
@@ -1,7 +1,14 @@
 import yaml
+import json
 
 with open ("app.yaml", "r") as stream:
     try:
-        print(yaml.load(stream))
+        menu = yaml.load(stream)
+        print(menu["name"])
+        print(menu["location"])
+        print("=========")
+        print("Menu")
+        for meal in menu["menu"]:
+            print(meal["name"]+ " - £" + str(meal["price"]))
     except yaml.YAMLError as exc:
         print(exc)
